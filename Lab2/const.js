626dbf916454f0bf3303851c2ceecd054725806a
Gamepro5 <gamepro543@gmail.com>
2024-02-06 20:47:44 -0800

    Lab2 Done

---
+++ Lab2/const.js
@@ -0,0 +1,16 @@
+document.querySelector(".header").innerHTML = `
+<div class="title">
+  <p class="title-text">Cat Hub</p>
+</div>`;
+
+document.querySelector(".navbar").innerHTML = `
+  <a class="nav-button" href="/">Home</a>
+  <a class="nav-button" href="/about">About</a>
+  <a class="nav-button" href="/media">Media</a>
+  <a class="nav-button" href="/stats">Stats</a>`;
+
+document.querySelector(".footer").innerHTML = `
+<center>
+  <p><a class="gp5-logo" href="https://gamepro5.com"><img src="https://gamepro5.com/images/gamepro5.svg"></a></p>
+  <p>Website made by Gamepro5.</p>
+</center>`;
